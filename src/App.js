32e84727f273a12cd9995cdc2316ba9409944c86
Isaí <isai.hernandez@peisa.mx>
2021-06-24 09:32:24 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+//import logo from "./logo.svg";
+import "./App.css";
+import ConditionalSection from "./sections/conditional";
+class Title extends Component {
+  render() {
+    return <h1>{this.props.text}</h1>;
+  }
+}
 
+Title.defaultProps = {
+  text: "Título por defecto",
+};
+
+class Contador extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { contador: this.props.contadorInicial };
+    setInterval(() => {
+      this.setState({ contador: this.state.contador + 1 });
+    }, 1000);
+  }
+  render() {
+    //const contador = 0;
+    //return <span>{this.state.contador}</span>;
+    return <ContadorNumero numero={this.state.contador} />;
+  }
+}
+Contador.defaultProps = {
+  contadorInicial: 0,
+};
+class ContadorNumero extends Component {
+  render() {
+    console.log("ContadorNumero render()");
+    return <span>{this.props.numero}</span>;
+  }
+}
 function App() {
+  const numbers = [1, 1, 3, 4, 5];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Title text="Otro título desde props" />
+      <p>Primer componente con state</p>
+      {numbers.map((number, index) => {
+        return <p key={index}>Soy el numero {number}</p>;
+      })}
     </div>
   );
 }
